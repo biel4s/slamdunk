01ce335f820317fe678f2cdf93c9330cf483b9fd
biel4s <kamilbielawski13@gmail.com>
2023-10-21 14:46:04 +0200

    Changed register-page path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { HomeComponent } from './home/home.component';
 import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
-import { RegisterPageComponent } from './auth/register-page/register-page.component';
+import { RegisterPageComponent } from './auth/pages/register-page/register-page.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
