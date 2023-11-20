2f5e6db89438643e64c908a9ea3b090bfb308f48
biel4s <kamilbielawski13@gmail.com>
2023-11-20 19:25:48 +0100

    Added news section

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
+import {NewsComponent} from "./news/news.component";
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [CommonModule, HomeComponent, AboutComponent],
+  imports: [CommonModule, HomeComponent, AboutComponent, NewsComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
---
+++ src/app/main-page/news/news.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-news',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './news.component.html',
+  styleUrl: './news.component.scss'
+})
+export class NewsComponent {
+
+}
