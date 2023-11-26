0b4dd28a1712dcfc81a9d0d208934d71934a54a0
biel4s <kamilbielawski13@gmail.com>
2023-11-26 20:06:33 +0100

    Fixed mat cards

--- src/app/main-page/news/pages/news-page.component.ts
+++ src/app/main-page/news/pages/news-page.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NewsCardComponent} from "../components/news-card/news-card.component";
 import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-news-page',
   standalone: true,
-  imports: [CommonModule, NewsCardComponent, MatIconModule],
+  imports: [CommonModule, NewsCardComponent, MatIconModule, MatButtonModule],
   templateUrl: './news-page.component.html',
   styleUrl: './news-page.component.scss'
 })
