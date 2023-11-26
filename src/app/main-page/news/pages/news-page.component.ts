ba539f60d407d6c4bfd66b4c2bfa2d1abf834428
biel4s <kamilbielawski13@gmail.com>
2023-11-26 15:47:42 +0100

    Added cards

---
+++ src/app/main-page/news/pages/news-page.component.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NewsCardComponent} from "../components/news-card/news-card.component";
+import {MatIconModule} from "@angular/material/icon";
+
+@Component({
+  selector: 'app-news-page',
+  standalone: true,
+  imports: [CommonModule, NewsCardComponent, MatIconModule],
+  templateUrl: './news-page.component.html',
+  styleUrl: './news-page.component.scss'
+})
+export class NewsPageComponent {
+  scrollToLeft() {
+
+  }
+
+  scrollToRight() {
+
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,14 @@
 export const environment = {
+  apiBasketballUrl: 'https://api-basketball.p.rapidapi.com',
+  apiBasketballHeaders: {
+    'X-RapidAPI-Key': '58c745bc4fmsh1ffecb00ff80e2dp1e932fjsn1ed5ca273f98',
+    'X-RapidAPI-Host': 'api-basketball.p.rapidapi.com'
+  },
+  apiLiveScoreUrl: 'https://livescore6.p.rapidapi.com',
+  apiLiveScoreHeaders: {
+    'X-RapidAPI-Key': '58c745bc4fmsh1ffecb00ff80e2dp1e932fjsn1ed5ca273f98',
+    'X-RapidAPI-Host': 'livescore6.p.rapidapi.com'
+  },
   production: false,
   firebase: {
     apiKey: "AIzaSyBNeHdhqTE8m5_HIm1fx15ffemg7K_nZFE",
