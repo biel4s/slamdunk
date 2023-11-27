754393db4301b6b9f47fc7e216e1487d6b22f973
biel4s <kamilbielawski13@gmail.com>
2023-11-27 13:40:46 +0100

    Changed header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import {CommonModule, NgOptimizedImage} from '@angular/common';
-import {Component, HostListener} from '@angular/core';
+import {Component} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
@@ -23,21 +23,10 @@ import {AuthService} from "../../../auth/auth.service";
   styleUrls: ['./header.component.scss'],
 })
 export class NavbarComponent {
-  navLinks = ['Home', 'About', 'News', 'Events', 'More'];
-  moreLinks = ['League', 'Players', 'Calendar', 'Standings', 'Contact'];
+  navLinks = ['Home', 'News', 'Leagues', 'Standings', 'Teams', 'Players', 'Calendar', 'About'];
   leagueLinks = ['EuroLeague', 'EuroCup', 'BCL', 'ACB', 'BSL', 'Lega Basket A']
   isScrolled: boolean = false;
 
   constructor(private router: Router, public authService: AuthService) {
-  }
-
-  @HostListener('window:scroll', [])
-
-  onWindowScroll(): void {
-    this.isScrolled = window.scrollY > 50;
-  }
-
-  scrollToSection(sectionId: string): void {
-    document.getElementById(sectionId.toLowerCase())?.scrollIntoView({behavior: 'smooth'});
   }
 }
