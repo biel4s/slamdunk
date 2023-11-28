743b3d25389dd1bb525b8cf1c238f2a36296d963
biel4s <kamilbielawski13@gmail.com>
2023-11-28 21:09:50 +0100

    Added event cards

--- src/app/events/components/events-card/events-card.component.ts
+++ src/app/events/components/events-card/events-card.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-events-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './events-card.component.html',
   styleUrl: './events-card.component.scss'
 })
