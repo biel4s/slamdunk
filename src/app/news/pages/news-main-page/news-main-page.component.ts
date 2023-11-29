0a29f2174a09380982e9a6ff74e52139e94432b7
biel4s <kamilbielawski13@gmail.com>
2023-11-29 16:43:37 +0100

    Added routing to read more buttons

--- src/app/news/pages/news-main-page/news-main-page.component.ts
+++ src/app/news/pages/news-main-page/news-main-page.component.ts
@@ -3,11 +3,12 @@ import {CommonModule} from '@angular/common';
 import {NewsCardComponent} from "../../components/news-card/news-card.component";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-news-page',
   standalone: true,
-  imports: [CommonModule, NewsCardComponent, MatIconModule, MatButtonModule],
+  imports: [CommonModule, NewsCardComponent, MatIconModule, MatButtonModule, RouterLink],
   templateUrl: './news-main-page.component.html',
   styleUrl: './news-main-page.component.scss'
 })
