49c230fc167ad8c34c765c58df575e05d374bebb
biel4s <kamilbielawski13@gmail.com>
2023-12-09 13:23:10 +0100

    Finished news page

--- src/app/news/pages/news-urls/filip-petrusev-returns-to-europe-page/filip-petrusev-returns-to-europe-page.component.ts
+++ src/app/news/pages/news-urls/filip-petrusev-returns-to-europe-page/filip-petrusev-returns-to-europe-page.component.ts
@@ -3,11 +3,12 @@ import {CommonModule} from '@angular/common';
 import {FooterComponent} from "../../../../shared/components/footer/footer.component";
 import {NavbarComponent} from "../../../../shared/components/header/header.component";
 import {NewsUrlCardComponent} from "../../../components/news-url-card/news-url-card.component";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-filip-petrusev-returns-to-europe-page',
   standalone: true,
-  imports: [CommonModule, FooterComponent, NavbarComponent, NewsUrlCardComponent],
+  imports: [CommonModule, FooterComponent, NavbarComponent, NewsUrlCardComponent, MatButtonModule],
   templateUrl: './filip-petrusev-returns-to-europe-page.component.html',
   styleUrl: './filip-petrusev-returns-to-europe-page.component.scss'
 })
