f60382384f078b4684a777d5240e3d3637ef9598
biel4s <kamilbielawski13@gmail.com>
2023-12-31 15:51:41 +0100

    Added sidenav to header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import {CommonModule, NgOptimizedImage} from '@angular/common';
-import {Component, HostListener} from '@angular/core';
+import {Component, HostListener, ViewChild} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {Router, RouterLinkActive} from "@angular/router";
+import {MatDrawer, MatSidenavModule} from "@angular/material/sidenav";
+import {RouterLinkActive} from "@angular/router";
 import {LoginPageStore} from "../../../auth/login-page/login-page.store";
 import {AuthService} from "../../../auth/services/auth.service";
 
@@ -16,25 +17,42 @@ import {AuthService} from "../../../auth/services/auth.service";
     CommonModule,
     MatMenuModule,
     MatToolbarModule,
+    MatSidenavModule,
     MatButtonModule,
     MatIconModule,
     NgOptimizedImage,
     RouterLinkActive,
+
   ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class NavbarComponent {
-  navLinks: string[] = ['Home', 'News', 'Events', 'Schedule', 'Stats', 'Standings', 'Teams', 'Players'];
-  isScrolled: boolean = false;
+  @ViewChild('drawer') drawer!: MatDrawer;
 
-  constructor(public authService: AuthService, private loginPageStore: LoginPageStore, private router: Router) {
+  navLinks: string[] = ['Home', 'News', 'Events', 'Schedule', 'Stats', 'Standings', 'Teams', 'Players'];
+  isScreenSmall: boolean = false;
+
+  constructor(public authService: AuthService) {
+    this.isScreenSmall = window.innerWidth <= 1300;
   }
 
-  @HostListener('window:scroll', [])
+  @HostListener('window:resize', [])
+  onWindowResize() {
+    this.isScreenSmall = window.innerWidth <= 1300;
+    if (!this.isScreenSmall && this.drawer.opened) {
+      this.drawer.close().then();
+    }
+  }
 
-  onWindowScroll(): void {
-    this.isScrolled = window.scrollY > 50;
+  toggleDrawer() {
+    const drawerWidth = this.drawer._getWidth();
+    this.drawer.toggle().then();
+    if (this.drawer.opened) {
+      document.querySelector('.header-menu')!.classList.add('open');
+    } else {
+      document.querySelector('.header-menu')!.classList.remove('open');
+    }
   }
 
   onLogout(): void {
