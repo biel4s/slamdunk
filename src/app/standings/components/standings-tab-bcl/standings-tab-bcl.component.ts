1bbaa47c0824c3a2af9227c9f1b2f55f1167b3fa
biel4s <kamilbielawski13@gmail.com>
2024-05-13 19:38:23 +0200

    Implemented sorting function on standings

--- src/app/standings/components/standings-tab-bcl/standings-tab-bcl.component.ts
+++ src/app/standings/components/standings-tab-bcl/standings-tab-bcl.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, ViewChild} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
-import {MatTableModule} from "@angular/material/table";
+import {MatTableDataSource, MatTableModule} from "@angular/material/table";
 import {MatTabsModule} from "@angular/material/tabs";
 import {StandingsModel} from "../../models/standings.model";
 import {MatSort, MatSortModule, Sort} from "@angular/material/sort";
@@ -15,15 +15,17 @@ import {LiveAnnouncer} from "@angular/cdk/a11y";
 })
 export class StandingsTabBCLComponent {
   @Input() standingsSource!: StandingsModel[];
+  dataSource!: MatTableDataSource<StandingsModel>;
   displayedColumns: string[] = ['position', 'team', 'wins', 'loses', 'winratio', 'last5', 'points'];
   @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private _liveAnnouncer: LiveAnnouncer) {
   }
 
-  /*ngAfterViewInit() {
-    this.standingsSource.sort = this.sort;
-  }*/
+  ngAfterViewInit() {
+    this.dataSource = new MatTableDataSource(this.standingsSource);
+    this.dataSource.sort = this.sort;
+  }
 
   announceSortChange(sortState: Sort) {
     if (sortState.direction) {
